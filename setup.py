9bd526f15e672ba2682106b2ba32fd65b4921537
Alexandre Erwin Ittner <alexandre@ittner.com.br>
2009-08-15 07:39:53 +0000

    Version bump

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name="sked",
-    version="0.1",
+    version="0.2",
     description="The wikish scheduler",
     author="Alexandre Erwin Ittner",
     author_email="alexandre@ittner.com.br",

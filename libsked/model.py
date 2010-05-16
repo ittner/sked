c45062d49ceaf8d1b424bdc9c0afb09ee1dd9773
Alexandre Erwin Ittner <alexandre@ittner.com.br>
2010-05-15 21:28:53 -0300

    Some changes on the new model and API for pages. Work in progress.

--- libsked/model.py
+++ libsked/model.py
@@ -98,27 +98,77 @@ class OptionManager:
 
 class Page(object):
     
-    def __init__(self, db, name = None)
-        self._db = db
-        if name != None:
-            self.load(name)
-        else:
-            self.name = u""
-            self.normalized_name = u""
-            self.text = u""
-            self.curpos = 0
+    def __init__(self, name = None, text = None):
+        """ Creates a new page entry with default values. """
+        self._cursor_pos = 0
+        self.name = name or u""
+        self.text = text or u""
 
-    def load(self, pagename)
+    @staticmethod
+    def set_db(db_object):
+        """ Sets a class-level database handler. This method MUST be called
+        before any database operation. """
+        Page._db = db_object
+        return
+
+    @staticmethod
+    def exists(pagename):
+        """ Returns True if the database have a page with the given name. """
         pass
 
-    def save(self, name)
+    def load(self, pagename):
+        """ Loads the page given by 'pagename' into *this* instance. All the
+        current values will be overwritten. """
+        pass
+
+    def save(self):
+        """ Saves the current page to the database. Name is always taken from
+        the 'name' property. """
         if self.text == None or self.text == u"":
-            self.delete(name)
+            self.delete()
         else:
             pass
 
-    def delete(self, name)
+    def delete(self):
+        """ Deletes the current page from the database. """
         pass
 
-    def _normalize_name(self):
-        pass
+    @staticmethod
+    def _normalize_name(name):
+        n = u"page:" + name.strip().lower()
+        return n.encode("utf-8")
+
+    def _set_name(self, name):
+        self._name = name
+        self.normalized_name = Page._normalize_name(name)
+
+    def _get_name(self):
+        return self._name
+
+    name = property(_get_name, _set_name)
+
+    def _revalidate_cursor_pos(self):
+        """ Forces the cursor position to the text limits """
+        if self._cursor_pos < 0:
+            self._cursor_pos = 0
+        l = len(self.text)
+        if self._cursor_pos > l:
+            self._cursor_pos = l
+
+    def _get_cursor_pos(self):
+        return self._cursor_pos or 0
+
+    def _set_cursor_pos(self, position):
+        self._cursor_pos = position
+        self._revalidate_cursor_pos()
+
+    cursor_pos = property(_get_cursor_pos, _set_cursor_pos)
+
+    def _set_text(self, text):
+        self._text = text
+        self._revalidate_cursor_pos()
+
+    def _get_text(self):
+        return self._text
+
+    text = property(_get_text, _set_text)

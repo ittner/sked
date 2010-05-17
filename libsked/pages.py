26a2bb00f92580caf3381725c47a108e2a87be12
Alexandre Erwin Ittner <alexandre@ittner.com.br>
2010-05-17 15:20:45 -0300

    Separating page and option management modules.

--- libsked/pages.py
+++ libsked/pages.py
@@ -19,82 +19,8 @@
 # MA 02111-1307, USA.
 
 """
-Main control classes
+Page management module.
 """
-
-class OptionManager:
-    """
-    Class for handling appliation's options.
-    """
-
-    def __init__(self, db, defaults = {}):
-        self._db = db
-        self._opts = {}
-        self.set_defaults(defaults)
-        self.load()
-
-    def set_defaults(self, defaults):
-        self._defs = {}
-        for k in defaults:
-            self._defs[k] = defaults[k]
-            
-    def save(self):
-        self._db.set_key("options", self._opts)
-    
-    def load(self):
-        s = self._db.get_key("options")
-        if s:
-            self._opts = s
-
-    def get_str(self, key):
-        if self._opts.has_key(key):
-            return self._opts[key]
-        elif self._defs.has_key(key):
-            return self._defs[key]
-        else:
-            return None
-    
-    def set_str(self, key, value):
-        self._opts[key] = value
-
-    def get_int(self, key):
-        s = self.get_str(key)
-        if s != None:
-            return int(s)
-        return None
-
-    def set_int(self, key, value):
-        self.set_str(key, "%d" % value)
-
-    def get_bool(self, key):
-        v = self.get_int(key)
-        if v != None:
-            if v != 0:
-                return True
-            else:
-                return False
-        else:
-            return None
-
-    def set_bool(self, key, value):
-        if value == True:
-            self.set_int(key, 1)
-        else:
-            self.set_int(key, 0)
-            
-    def get_color(self, key):
-        try:
-            c = gdk.color_parse(self.get_str(key))
-        except ValueError:
-            if self._defs.has_key(key):
-                c = gdk.color_parse(self._defs[key])    # for invalid colors.
-        return c
-
-    def set_color(self, key, color):
-        self.set_str(key, "#%.2X%.2X%.2X" %
-            (color.red/256, color.green/256, color.blue/256))
-
-
 
 class PageManager(object):
     _ENCODING = "utf-8"

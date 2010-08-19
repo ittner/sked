d534071277ec280022c5f14a3d763789aeed774d
Alexandre Erwin Ittner <alexandre@ittner.com.br>
2010-08-19 09:04:56 -0300

    Random changes in the documentation files

--- setup.py
+++ setup.py
@@ -13,6 +13,20 @@ setup(name="sked",
 Sked is a personal organizer, calendar, schedule and braindump application
 with a wiki-like syntax for text formatting and organization. It is easier
 think of it as the result of the merge of a calendar with a desktop wiki.
+
+Sked is designed to optimize your workflow and everything is tailored to speed
+up editing operations: links and styles are unobtrusively inferred from the
+syntax, entries are automatically saved after a few seconds, almost all
+commands have keyboard shortcuts, and the last entries are kept in a history.
+Also, the wiki code is *not* hidden from the editing interface -- this makes
+editing easier, avoids the create/view/change cycle, and allows you to copy
+and paste text among other applications without loss of information. An
+user-defined delay is given before the text reformatting, so its appearance
+will not change while you are typing.
+
+All entries are stored in a compressed and encrypted database in your $HOME
+directory. The encryption aims protect the information even from a skilled
+adversary.
 """,
     packages=["libsked"],
     package_data={"libsked": [ "*.ui", "sked.dtd", "help.xml", "sked.png" ] },

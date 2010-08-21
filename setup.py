8f31ae57cf4eee5b5b0545459f21d6627f6dfc12
Alexandre Erwin Ittner <alexandre@ittner.com.br>
2010-08-21 15:51:36 -0300

    Put a hook into the build process to generate a manual page

--- setup.py
+++ setup.py
@@ -1,7 +1,52 @@
 #!/usr/bin/env python
 
 from distutils.core import setup
+from  distutils.command import build
 import libsked
+import os
+from datetime import datetime
+
+class SkedBuilder(build.build):
+    """ Put a hook into the build process to care of our own needs. """
+
+    def run(self):
+        self.sked_to_man("README", "sked", 1, "Sked", True)
+        build.build.run(self)
+
+    def sked_to_man(self, fromfile, pagename, group, title, forcename=False):
+        ifp = open(fromfile, "r")
+        dt = datetime.fromtimestamp(os.stat(fromfile).st_mtime)
+        ofname = pagename + "." + str(group)
+        ofp = open(ofname, "w")
+        ofp.write('.TH %s %i "%04d-%02d-%02d" "%s" "%s Manual Page"\n' %
+            (pagename, group, dt.year, dt.month, dt.day, title, title))
+        if forcename:
+            ofp.write(".SH NAME\n")
+        first_sec = True
+        while True:
+            line = ifp.readline()
+            if not line:
+                break
+            sline = line.strip()
+            if sline == "":
+                ofp.write(".PP\n")
+            elif line[0] == "=":
+                title = sline.strip("= ")
+                if sline.startswith("=="):
+                    title = title.upper()
+                if not (first_sec and title == fromfile.upper()):
+                    ofp.write(".SH " + title + "\n")
+                first_sec = False
+            else:
+                if line[0] == " " and not sline[0].isalpha():
+                    ofp.write(".br\n")
+                ofp.write(sline)
+                ofp.write("\n")
+        ifp.close()
+        ofp.close()
+        print("Manpage " + ofname + " generated from " + fromfile)
+        return ofname
+
 
 setup(name="sked",
     version=libsked.VERSION,
@@ -35,6 +80,8 @@ adversary.
     data_files=[
         ('share/doc/sked', ["README", "COPYING"]),
         ('share/applications', ["sked.desktop"]),
-        ('share/pixmaps', ["libsked/sked.png"])
-    ]
+        ('share/pixmaps', ["libsked/sked.png"]),
+        ('share/man/man1/', ["sked.1"])
+    ],
+    cmdclass={'build': SkedBuilder}
 )
